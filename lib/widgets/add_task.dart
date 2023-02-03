0790d6ef8b32593465f75d37f71788718ac023bb
Rishabh Sharma <rishabhsharma28302@gmail.com>
2023-02-03 16:43:41 +0530

    RS//Added Priority

--- lib/widgets/add_task.dart
+++ lib/widgets/add_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Task extends StatelessWidget {
   const Task({super.key});
@@ -24,7 +25,53 @@ class Task extends StatelessWidget {
                 border: OutlineInputBorder(),
                 hintText: "Add a task",
                 hintStyle: TextStyle(color: Colors.white)),
-          )
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Priority:",
+            style: TextStyle(
+                fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RatingBar.builder(
+            initialRating: 3,
+            minRating: 1,
+            direction: Axis.horizontal,
+            allowHalfRating: true,
+            itemCount: 5,
+            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+            itemBuilder: (context, _) => Icon(
+              Icons.star,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            onRatingUpdate: (rating) {
+              print(rating);
+            },
+          ),
+          SizedBox(
+            height: 190,
+          ),
+          Row(
+            children: [
+              MaterialButton(
+                onPressed: () {},
+                child: Text("Save"),
+                color: Color.fromARGB(255, 123, 123, 123),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Text("Cancel"),
+                color: Color.fromARGB(255, 123, 123, 123),
+              ),
+            ],
+          ),
         ]),
       ),
     );
