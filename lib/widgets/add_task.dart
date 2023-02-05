4d9ad33d8bf6fa2f7c19c385d818434b40277dfb
Vishal-Sankar <vishalsankar2002.gmail.com>
2023-02-05 22:55:16 +0530

    vs//fixed pop-up

--- lib/widgets/add_task.dart
+++ lib/widgets/add_task.dart
@@ -12,71 +12,77 @@ class Task extends StatelessWidget {
     return AlertDialog(
       backgroundColor: Color.fromARGB(255, 44, 44, 44),
       content: Container(
-        height: MediaQuery.of(context).size.height / 3,
-        width: MediaQuery.of(context).size.width * 0.75,
-        child: Column(children: [
-          Text(
-            "ADD TASK",
-            style: TextStyle(
-                fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Add a task",
-                hintStyle: TextStyle(color: Colors.white)),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Priority:",
-            style: TextStyle(
-                fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RatingBar.builder(
-            initialRating: 3,
-            minRating: 1,
-            direction: Axis.horizontal,
-            allowHalfRating: true,
-            itemCount: 5,
-            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-            itemBuilder: (context, _) => Icon(
-              Icons.star,
-              color: Color.fromARGB(255, 255, 255, 255),
+        height: MediaQuery.of(context).size.height / 2,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "ADD TASK",
+              style: TextStyle(
+                  fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
             ),
-            onRatingUpdate: (rating) {
-              print(rating);
-            },
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.06,
-          ),
-          Row(
-            children: [
-              MaterialButton(
-                onPressed: save,
-                child: Text("Save"),
-                color: Color.fromARGB(255, 123, 123, 123),
+            // SizedBox(
+            //   height: 10,
+            // ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: controller,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Add a task",
+                  hintStyle: TextStyle(color: Colors.white)),
+            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
+            const Text(
+              "Priority:",
+              style: TextStyle(
+                  fontSize: 24, color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
+            RatingBar.builder(
+              initialRating: 3,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.25,
-              ),
-              MaterialButton(
-                onPressed: cancel,
-                child: Text("Cancel"),
-                color: Color.fromARGB(255, 123, 123, 123),
-              ),
-            ],
-          ),
-        ]),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            ),
+            // SizedBox(
+            //   height: MediaQuery.of(context).size.height * 0.06,
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(
+                  onPressed: save,
+                  child: Text("Save"),
+                  color: Color.fromARGB(255, 123, 123, 123),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                ),
+                MaterialButton(
+                  onPressed: cancel,
+                  child: Text("Cancel"),
+                  color: Color.fromARGB(255, 123, 123, 123),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
